1e6ca5cfaa3d6aa76fcb2df32a7e3d43512b1e87
mjh-TU <mjh8018@utulsa.edu>
2025-02-13 19:49:52 -0600

    Added notes for part 2 and renamed files

---
+++ python/suspiciousloginmonitoring.py
@@ -0,0 +1,8 @@
+# Write script to
+#
+# Parse system logs for failed login attempts
+# Identify repeated login failures from the same IP
+# Alert the user if a threshold is exceeded
+# Monitor privileged account Logins
+
+# Then finally simulate failed logins and observe how the script detects them
